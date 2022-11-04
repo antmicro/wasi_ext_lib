37c5664e5e6dc5bdc363d2155633464096b39958
Grzegorz Placzek <gplaczek@antmicro.com>
2022-11-04 15:24:51 +0100

    Rename c directory and add Makefile

--- build.rs
+++ build.rs
@@ -1,30 +1,32 @@
 extern crate bindgen;
+
 use std::env;
+use std::process::Command;
+
+const CLIB_DIR: &str = "c_lib";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=c_bindings/wasi_ext_lib.c");
-    println!("cargo:rerun-if-changed=c_bindings/wasi_ext_lib.h");
-    cc::Build::new()
-        .archiver(format!("{}/bin/ar", env!("WASI_SDK_PATH")))
-        .file("c_bindings/wasi_ext_lib.c")
-        .file("c_bindings/json/json.c")
-        .flag("-DHTERM")
-        .flag("-Wall")
-        .flag("-Wextra")
-        .compile("wasi_ext_lib");
+    println!("cargo:rerun-if-changed={}/wasi_ext_lib.c", CLIB_DIR);
+    println!("cargo:rerun-if-changed={}/wasi_ext_lib.h", CLIB_DIR);
+    Command::new("make")
+        .arg("-C")
+        .arg(CLIB_DIR)
+        .spawn()
+        .expect("Could not build C library");
+    println!("cargo:rustc-link-search={}/bin/", CLIB_DIR);
 
     println!("cargo:rustc-link-lib=static=wasi_ext_lib");
-    println!("cargo:rerun-if-changed=c_bindings/wasi_ext_lib.h");
+    println!("cargo:rerun-if-changed={}/wasi_ext_lib.h", CLIB_DIR);
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=c_bindings/wasi_ext_lib.c");
+    println!("cargo:rerun-if-changed={}/wasi_ext_lib.c", CLIB_DIR);
     println!("cargo:rerun-if-changed=src/lib.rs");
     bindgen::Builder::default()
-        .header("c_bindings/wasi_ext_lib.h")
+        .header(format!("{}/wasi_ext_lib.h", CLIB_DIR))
         .clang_arg(format!("--sysroot={}/share/wasi-sysroot", env!("WASI_SDK_PATH")))
         .clang_arg("-DHTERM")
         .clang_arg("-fvisibility=default")
-        .allowlist_file("c_bindings/wasi_ext_lib.h")
+        .allowlist_file(format!("{}/wasi_ext_lib.h", CLIB_DIR))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings")
