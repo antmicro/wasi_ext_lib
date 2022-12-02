3b5c6a561529d82f07c09ed10a65a96ef5f86eca
Krzysztof Juszczyk <kjuszczyk@internships.antmicro.com>
2022-12-02 15:34:01 +0100

    Add event mask size constant

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,6 @@
 pub type Wasi_events = u32;
 pub const WASI_EVENTS_NUM: usize = 1;
+pub const WASI_EVENTS_MASK_SIZE: usize = 4; // number of bytes
 
 // Hterm events
 pub const WASI_EVENT_WINCH: Wasi_events = 1 << 0;
