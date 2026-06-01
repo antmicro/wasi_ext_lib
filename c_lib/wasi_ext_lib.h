11fb726d55932f5e58dc3fa34a29dea5982855dd
Dawid Kaczmarzyk <dkaczmarzyk@internships.antmicro.com>
2026-06-01 13:05:53 +0200

    [#94697] Comment formatting

--- c_lib/wasi_ext_lib.h
+++ c_lib/wasi_ext_lib.h
@@ -125,8 +125,9 @@ int wasi_ext_spawn(const char *, const char *const *, size_t,
 int wasi_ext_kill(int, int);
 
 /**
-  * @brief Custom ioctl syscall implementation.
-  * @return 0 on success, or a positive error code on failure (note the negation).
+ * @brief Custom ioctl syscall implementation.
+ * @return 0 on success, or a positive error code on failure
+ * (note the negation).
  */
 int wasi_ext_ioctl(int, unsigned int, void *);
 int wasi_ext_fcntl(int, enum FcntlCommand, void *);
